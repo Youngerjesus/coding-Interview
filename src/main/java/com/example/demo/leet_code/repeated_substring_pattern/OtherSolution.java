8b39722f6f88c596b288c2517e29ff7d258deee2
Youngerjesus <yjh930919@gmail.com>
2021-07-16 23:54:37 +0900

    Add Repeated SubString pattern

--- src/main/java/com/example/demo/leet_code/repeated_substring_pattern/OtherSolution.java
+++ src/main/java/com/example/demo/leet_code/repeated_substring_pattern/OtherSolution.java
@@ -2,6 +2,22 @@ package com.example.demo.leet_code.repeated_substring_pattern;
 
 public class OtherSolution {
     public boolean repeatedSubstringPattern(String s){
+        int L = s.length();
+        for (int i = 1; i < L; i++) {
+            String pattern = s.substring(0, i);
+            int M = pattern.length();
+            int j = i;
+            for (;j < L && j + M <= L; j += M) {
+                String substring = s.substring(j, j + pattern.length());
+                if (!substring.equals(pattern)) {
+                    break;
+                }
+            }
+
+            if (j == L) {
+                return true;
+            }
+        }
         return false;
     }
 }
